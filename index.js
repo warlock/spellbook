874895f3b24eac990b413e35b7fdb0903a73285e
Josep Subils <ninpo85@gmail.com>
2015-06-17 16:45:22 +0200

    clean console.log

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ exports.whois = function (domini, callback) {
 	query.stdout.setEncoding('utf8');
 	query.stdout.on('data', function (data) {
                 var str = data.toString();
-		//var lines = str.split(/(\r?\n)/g);
 		var lines = str.split("\n");
                 for (var i in lines) {
 			if (lines[i].indexOf(':') != -1) {
-				//console.log(lines[i]);
 				domini.push(lines[i].split(":"));
 			}
 		}
-		console.log(domini);
 	});
 
 	query.on('close', function () {
