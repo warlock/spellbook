63e3ceaaa542f0260ce056305a6958e62d466f14
Josep Subils <ninpo85@gmail.com>
2015-06-17 17:38:05 +0200

    updated array to object

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ var spawn = require('child_process').spawn;
 
 exports.whois = function (domini, callback) {
 	var query = spawn('whois',  [domini]);
-	var domini = [];
+	var domini = {};
 	query.stdout.setEncoding('utf8');
 	query.stdout.on('data', function (data) {
                 var str = data.toString();
 		var lines = str.split("\n");
                 for (var i in lines) {
 			if (lines[i].indexOf(':') != -1) {
-				domini.push(lines[i].split(":"));
+				var cut = lines[i].split(":");
+				if (cut[0] != undefined && cut[1] != undefined) {
+					domini[cut[0]] = cut[1];
+				}
 			}
 		}
 	});
 
-	query.on('close', function () {
+	query.on('close', function (code) {
                 callback(null, domini);
         });
 
