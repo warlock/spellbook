6b28e8c2cb3351e3c795528ea98a3f1569c0d667
Josep Subils <ninpo85@gmail.com>
2015-06-23 10:05:04 +0200

    Last update

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ exports.whois = function (domini, callback) {
 				var cut = lines[i].split(":");
 				var c1 = cut[0].replace(/^\s+/g,'').replace(/\s+$/g,'');
 				var c2 = cut[1].replace(/^\s+/g,'').replace(/\s+$/g,'');
-
 				if (c1 != undefined && c2 != undefined) {
-					domini[cut[0]] = cut[1];
+					domini[c1] = c2;
 				}
 			}
 		}
