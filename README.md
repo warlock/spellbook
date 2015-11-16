fe5b76a0ffbdb1e220b79b0aa2b0101db8d1cee2
Josep Subils <jsubils@anpro21.com>
2015-11-16 10:09:23 +0100

    getDomain

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SpellBook
 Warning: Development version.
+
+Import module:
+
+var sp = require("spellbook");
+
+
+Array.deleteOf('element');
+
+var list = ['a', 'b', 'c'];
+
+list.deleteOf('b');
+
+-> ['a', 'c']
+
+Domain extractor:
+sp.getDomain("http://www.dondominio.com");
+
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-Array.prototype.deleteElement = function (element) {
-	this.splice(hola.indexOf(element),1);
+Array.prototype.deleteOf = function (element) {
+	return this.splice(this.indexOf(element),1);
 }
+
+
+var Spellbook = function () {
+	this.getDomain = function(url) {
+		if (url.indexOf("://") > -1) {
+			url = url.split('/')[2];
+		} else {
+			url = url.split('/')[0];
+		}
+		url = url.split(':')[0];
+		return url.replace('www.','');
+	}
+}
+
+
+module.exports = new Spellbook();
