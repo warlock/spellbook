2df5d9cb4895dcf0e89b76548f00e3d5695295f7
Josep Subils <jsubils@anpro21.com>
2015-11-16 09:25:55 +0100

    Array.deleteElement()

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-var spawn = require('child_process').spawn;
-
-exports.whois = function (domini, callback) {
-	var query = spawn('whois',  [domini]);
-	var domini = {};
-	query.stdout.setEncoding('utf8');
-	query.stdout.on('data', function (data) {
-                var str = data.toString();
-		var lines = str.split("\n");
-                for (var i in lines) {
-			if (lines[i].indexOf(':') != -1) {
-				var cut = lines[i].split(":");
-				var c1 = cut[0].replace(/^\s+/g,'').replace(/\s+$/g,'');
-				var c2 = cut[1].replace(/^\s+/g,'').replace(/\s+$/g,'');
-				if (c1 != undefined && c2 != undefined) {
-					domini[c1] = c2;
-				}
-			}
-		}
-	});
-
-	query.on('close', function (code) {
-        callback(null, domini);
-    });
-
-};
-
+Array.prototype.deleteElement = function (element) {
+	this.splice(hola.indexOf(element),1);
+}
