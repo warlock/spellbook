e77aa986836e7362648eadfa611767e49c25a3ca
Josep Subils <octozon@gmail.com>
2015-12-11 23:08:38 +0100

    deleted getDomain function

--- spellbook.js
+++ spellbook.js
@@ -23,17 +23,6 @@ if (!Array.prototype.clear) {
 /* Spellbook Utils */
 
 var Spellbook = function () {
-	this.getDomain = function(url) {
-		if (url.indexOf("://") > -1) {
-			url = url.split('/')[2];
-		} else {
-			url = url.split('/')[0];
-		}
-		url = url.split(':')[0];
-		return url.replace('www.','');
-	}
-
-	// Range by kennebec
 	this.range = function(a, b, step) {
     		var A= [];
     		if(typeof a == 'number'){
