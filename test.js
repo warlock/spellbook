78fc90c5eae28208449a8a988c0c1d4a735ae266
Josep Subils <jsubils@anpro21.com>
2015-12-14 10:53:54 +0100

    update

--- test.js
+++
@@ -1,6 +0,0 @@
-var sb = require("./spellbook");
-var objecte = new Date();
-var hola = ['1', 2, 3, 4, 5, objecte];
-console.log(hola);
-console.log(hola.remove(['1',2, 4, objecte]));
-console.log(hola.remove('1'));
