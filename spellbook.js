7b8b18238b0f62512009c62e286593ead8a0cfda
Josep Subils <jsubils@anpro21.cm>
2016-07-08 12:49:28 +0200

    add: making tests

--- spellbook.js
+++ spellbook.js
@@ -2,15 +2,14 @@
 var Spellbook = function () {
 	this.range = function (a, b, step) {
 		var A = []
-		if(typeof a == 'number'){
+		if (this.empty(a) || this.empty(b)) return A
+		if (typeof a == 'number') {
 			A[0] = a
 			step = step || 1
-			while(a+step<= b) {
-				A[A.length] = a += step
-			}
+			while(a+step<= b) A[A.length] = a += step
 		} else {
 			var s = 'abcdefghijklmnopqrstuvwxyz'
-			if(a === a.toUpperCase()) {
+			if (a === a.toUpperCase()) {
 				b = b.toUpperCase()
 				s = s.toUpperCase()
 			}
