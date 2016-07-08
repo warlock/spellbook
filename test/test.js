be24eeb69e3beb0cdfc31fc34d4cc5e9b578827d
Josep Subils <jsubils@anpro21.cm>
2016-07-08 13:45:28 +0200

    add: making tests

---
+++ test/test.js
@@ -0,0 +1,64 @@
+//var assert = require('chai').assert
+var chai = require('chai')
+var sb = require('../spellbook')
+
+describe('Tools', () => {
+	describe('#range()', () => {
+		it('Return array', () => {
+			chai.assert.typeOf(sb.range(), 'array')
+			chai.assert.lengthOf(sb.range(), 0)
+		})
+
+		it('Return array of numbers: 0, 1000', () => {
+			var res = sb.range(0,1000)
+			chai.assert.typeOf(res, 'array')
+			chai.assert.lengthOf(res, 1001)
+			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'number')
+		})
+
+		it('Return array of leters: a, z', () => {
+			var res = sb.range('a','z')
+			chai.assert.typeOf(res, 'array')
+			chai.assert.lengthOf(res, 26)
+			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
+		})
+
+		it('Return array of leters: A, Z', () => {
+			var res = sb.range('A','Z')
+			chai.assert.typeOf(res, 'array')
+			chai.assert.lengthOf(res, 26)
+			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
+		})
+/*
+		it('Testing combinations: 1, Z', () => {
+			var res = sb.range(1,'Z')
+			assert.typeOf(res, 'array')
+			assert.lengthOf(res, 26)
+			for (let i = 0;i<res.length; i++) assert.typeOf(res[i], 'string')
+		})
+*/
+	})
+
+	describe('#isFunction()', () => {
+		it('Check function', () => {
+			chai.assert.isTrue(sb.isFunction(() => {}), "Is function")
+		})
+
+		it('Check empty', () => {
+			chai.assert.isNotTrue(sb.isFunction(), "Is empty")
+		})
+
+		it('Check array', () => {
+			chai.assert.isNotTrue(sb.isFunction([]), "Is array")
+		})
+
+		it('Check string', () => {
+			chai.assert.isNotTrue(sb.isFunction(""), "Is string")
+		})
+
+		it('Check number', () => {
+			chai.assert.isNotTrue(sb.isFunction(1), "Is number")
+		})
+	})
+
+})
