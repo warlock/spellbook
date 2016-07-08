81dcb20af28a9e5783d700c28b3c97755b2d8535
Josep Subils <jsubils@anpro21.cm>
2016-07-08 13:57:26 +0200

    add: making tests

--- test/test.js
+++ test/test.js
@@ -40,24 +40,72 @@ describe('Tools', () => {
 	})
 
 	describe('#isFunction()', () => {
-		it('Check function', () => {
+		it('Check is function', () => {
 			chai.assert.isTrue(sb.isFunction(() => {}), "Is function")
 		})
 
-		it('Check empty', () => {
+		it('Check empty is not true', () => {
 			chai.assert.isNotTrue(sb.isFunction(), "Is empty")
 		})
 
-		it('Check array', () => {
+		it('Check array is not true', () => {
 			chai.assert.isNotTrue(sb.isFunction([]), "Is array")
 		})
 
-		it('Check string', () => {
+		it('Check string is not true', () => {
 			chai.assert.isNotTrue(sb.isFunction(""), "Is string")
 		})
 
-		it('Check number', () => {
+		it('Check number is not true', () => {
 			chai.assert.isNotTrue(sb.isFunction(1), "Is number")
+		})
+	})
+
+	describe('#isArray()', () => {
+		it('Check is array', () => {
+			chai.assert.isTrue(sb.isArray([]), "Is array")
+		})
+
+		it('Check function is not true', () => {
+			chai.assert.isNotTrue(sb.isArray(() => {}), "Is function")
+		})
+
+		it('Check empty is not true', () => {
+			chai.assert.isNotTrue(sb.isArray(), "Is empty")
+		})
+
+		it('Check string is not true', () => {
+			chai.assert.isNotTrue(sb.isArray(""), "Is string")
+		})
+
+		it('Check number is not true', () => {
+			chai.assert.isNotTrue(sb.isArray(1), "Is number")
+		})
+	})
+
+	describe('#isObject()', () => {
+		it('Check is object', () => {
+			chai.assert.isTrue(sb.isObject({}), "Is object")
+		})
+
+		it('Check is array', () => {
+			chai.assert.isNotTrue(sb.isObject([]), "Is array")
+		})
+
+		it('Check function is not true', () => {
+			chai.assert.isNotTrue(sb.isObject(() => {}), "Is function")
+		})
+
+		it('Check empty is not true', () => {
+			chai.assert.isNotTrue(sb.isObject(), "Is empty")
+		})
+
+		it('Check string is not true', () => {
+			chai.assert.isNotTrue(sb.isObject(""), "Is string")
+		})
+
+		it('Check number is not true', () => {
+			chai.assert.isNotTrue(sb.isObject(1), "Is number")
 		})
 	})
 
