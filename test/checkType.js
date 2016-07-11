707bbf9fcd6070fbda9c6c63881b67ee72df43f5
Josep Subils <jsubils@anpro21.cm>
2016-07-11 09:52:33 +0200

    update: travis.yml

--- test/checkType.js
+++ test/checkType.js
@@ -1,81 +1,81 @@
 var chai = require('chai')
 var sb = require('../spellbook')
 
-describe('Tools: Check Type', () => {
-	describe('#isFunction()', () => {
-		it('Check is function', () => {
-			chai.assert.isTrue(sb.isFunction(() => {}), "Is function")
+describe('Tools: Check Type', function () {
+	describe('#isFunction()', function () {
+		it('Check is function', function () {
+			chai.assert.isTrue(sb.isFunction(function () {}), "Is function")
 		})
 
-		it('Check empty is not true', () => {
+		it('Check empty is not true', function () {
 			chai.assert.isNotTrue(sb.isFunction(), "Is empty")
 		})
 
-		it('Check array is not true', () => {
+		it('Check array is not true', function () {
 			chai.assert.isNotTrue(sb.isFunction([]), "Is array")
 		})
 
-		it('Check string is not true', () => {
+		it('Check string is not true', function () {
 			chai.assert.isNotTrue(sb.isFunction(""), "Is string")
 		})
 
-		it('Check number is not true', () => {
+		it('Check number is not true', function () {
 			chai.assert.isNotTrue(sb.isFunction(1), "Is number")
 		})
 
-		it('Check object is not true', () => {
+		it('Check object is not true', function () {
 			chai.assert.isNotTrue(sb.isFunction({}), "Is object")
 		})
 	})
 
-	describe('#isArray()', () => {
-		it('Check is array', () => {
+	describe('#isArray()', function () {
+		it('Check is array', function () {
 			chai.assert.isTrue(sb.isArray([]), "Is array")
 		})
 
-		it('Check function is not true', () => {
-			chai.assert.isNotTrue(sb.isArray(() => {}), "Is function")
+		it('Check function is not true', function () {
+			chai.assert.isNotTrue(sb.isArray(function () {}), "Is function")
 		})
 
-		it('Check empty is not true', () => {
+		it('Check empty is not true', function () {
 			chai.assert.isNotTrue(sb.isArray(), "Is empty")
 		})
 
-		it('Check string is not true', () => {
+		it('Check string is not true', function () {
 			chai.assert.isNotTrue(sb.isArray(""), "Is string")
 		})
 
-		it('Check number is not true', () => {
+		it('Check number is not true', function () {
 			chai.assert.isNotTrue(sb.isArray(1), "Is number")
 		})
 
-		it('Check object is not true', () => {
+		it('Check object is not true', function () {
 			chai.assert.isNotTrue(sb.isArray({}), "Is object")
 		})
 	})
 
-	describe('#isObject()', () => {
-		it('Check is object', () => {
+	describe('#isObject()', function () {
+		it('Check is object', function () {
 			chai.assert.isTrue(sb.isObject({}), "Is object")
 		})
 
-		it('Check is array', () => {
+		it('Check is array', function () {
 			chai.assert.isNotTrue(sb.isObject([]), "Is array")
 		})
 
-		it('Check function is not true', () => {
-			chai.assert.isNotTrue(sb.isObject(() => {}), "Is function")
+		it('Check function is not true', function () {
+			chai.assert.isNotTrue(sb.isObject(function () {}), "Is function")
 		})
 
-		it('Check empty is not true', () => {
+		it('Check empty is not true', function () {
 			chai.assert.isNotTrue(sb.isObject(), "Is empty")
 		})
 
-		it('Check string is not true', () => {
+		it('Check string is not true', function () {
 			chai.assert.isNotTrue(sb.isObject(""), "Is string")
 		})
 
-		it('Check number is not true', () => {
+		it('Check number is not true', function () {
 			chai.assert.isNotTrue(sb.isObject(1), "Is number")
 		})
 	})
--- test/tools.js
+++ test/tools.js
@@ -2,39 +2,39 @@
 var chai = require('chai')
 var sb = require('../spellbook')
 
-describe('Tools', () => {
-	describe('#range()', () => {
-		it('Return array', () => {
+describe('Tools', function () {
+	describe('#range()', function () {
+		it('Return array', function () {
 			chai.assert.typeOf(sb.range(), 'array')
 			chai.assert.lengthOf(sb.range(), 0)
 		})
 
-		it('Return array of numbers: 0, 1000', () => {
+		it('Return array of numbers: 0, 1000', function () {
 			var res = sb.range(0,1000)
 			chai.assert.typeOf(res, 'array')
 			chai.assert.lengthOf(res, 1001)
-			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'number')
+			for (var i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'number')
 		})
 
-		it('Return array of leters: a, z', () => {
+		it('Return array of varers: a, z', function () {
 			var res = sb.range('a','z')
 			chai.assert.typeOf(res, 'array')
 			chai.assert.lengthOf(res, 26)
-			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
+			for (var i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
 		})
 
-		it('Return array of leters: A, Z', () => {
+		it('Return array of varers: A, Z', function () {
 			var res = sb.range('A','Z')
 			chai.assert.typeOf(res, 'array')
 			chai.assert.lengthOf(res, 26)
-			for (let i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
+			for (var i = 0;i<res.length; i++) chai.assert.typeOf(res[i], 'string')
 		})
 /*
-		it('Testing combinations: 1, Z', () => {
+		it('Testing combinations: 1, Z', function () {
 			var res = sb.range(1,'Z')
 			assert.typeOf(res, 'array')
 			assert.lengthOf(res, 26)
-			for (let i = 0;i<res.length; i++) assert.typeOf(res[i], 'string')
+			for (var i = 0;i<res.length; i++) assert.typeOf(res[i], 'string')
 		})
 */
 	})
