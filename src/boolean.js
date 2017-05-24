252485b13a0ae9c83de52c0c1d00df8501b2ba1a
Josep Subils <josep@java.cat>
2017-05-24 12:27:22 +0200

    [add] boolean comments

--- src/boolean.js
+++ src/boolean.js
@@ -1,14 +1,35 @@
 module.exports = {
-  'isTrue': function (data) {
-    return data === true;
+
+   /**
+   * @param {Boolean} bool : Get de boolean value.
+   * @returns {Boolean} : Return true if value is true.
+   */
+  'isTrue': function (bool) {
+    return bool === true;
   },
-  'isFalse': function (value) {
-    return value === false;
+
+   /**
+   * @param {Boolean} bool : Get de boolean value.
+   * @returns {Boolean} : Return true if value is false.
+   */
+  'isFalse': function (bool) {
+    return bool === false;
   },
+
+   /**
+   * @param {Any} value : Get any value.
+   * @param {Any} check : Get any value.
+   * @returns {Boolean} : Return result of comparision.
+   */
   'isEqual': function (value, check) {
     return JSON.stringify(value) === JSON.stringify(check);
   },
-  'Not': function (value) {
-    return !value;
+
+   /**
+   * @param {Boolean} bool : Get de boolean value.
+   * @returns {Boolean} : Return negative value.
+   */
+  'Not': function (bool) {
+    return !bool;
   }
 };
