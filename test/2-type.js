a5d40ce0d19e9a5250a9def49fbd110d96ecaacb
Josep Subils <josep@java.cat>
2017-05-24 14:30:48 +0200

    [test] add type tests

--- test/2-type.js
+++ test/2-type.js
@@ -2,21 +2,23 @@
 var chai = require('chai');
 var sb = require('../src/main');
 
-var data = {
-  "func": function () {
-    return 'hi';
-  },
-  "num": 1.2,
-  "int": 1,
-  "string": 'hi',
-  "obj": {"a": 'b'},
-  "bool": true,
-  "array": [1,2,3],
-  "null": null,
-  "undefined": undefined
-};
-
 describe('Type', function () {
+  var data = {
+    "func": function () {
+      //do nothing
+    },
+    "num": 1.2,
+    "int": 1,
+    "string": 'hi',
+    "obj": {"a": 'b'},
+    "bool": true,
+    "array": [1,2,3],
+    "null": null,
+    "undefined": undefined,
+    "emptarr": [],
+    "emptstr": ""
+  };
+
   describe('sb.isFunction()', function () {
     it('Check is function', function () {
       for (var key in data) {
@@ -30,7 +32,17 @@ describe('Type', function () {
     it('Check is Array', function () {
       for (var key in data) {
         if (key === 'array') chai.assert.isTrue(sb.isArray(data[key]));
+        else if (key === 'emptarr') chai.assert.isTrue(sb.isArray(data[key]));
         else chai.assert.isNotTrue(sb.isArray(data[key]));
+      }
+    });
+  });
+
+  describe('sb.isObject()', function () {
+    it('Check is Object', function () {
+      for (var key in data) {
+        if (key === 'obj') chai.assert.isTrue(sb.isObject(data[key]));
+        else chai.assert.isNotTrue(sb.isObject(data[key]));
       }
     });
   });
@@ -58,16 +70,8 @@ describe('Type', function () {
     it('Check is string', function () {
       for (var key in data) {
         if (key === 'string') chai.assert.isTrue(sb.isString(data[key]));
+        else if (key === 'emptstr') chai.assert.isTrue(sb.isString(data[key]));
         else chai.assert.isNotTrue(sb.isString(data[key]));
-      }
-    });
-  });
-
-  describe('sb.isObject()', function () {
-    it('Check is object', function () {
-      for (var key in data) {
-        if (key === 'obj') chai.assert.isTrue(sb.isObject(data[key]));
-        else chai.assert.isNotTrue(sb.isObject(data[key]));
       }
     });
   });
@@ -81,6 +85,17 @@ describe('Type', function () {
     });
   });
 
+  describe('sb.isEmpty()', function () {
+    it('Check is empty', function () {
+      for (var key in data) {
+        if (key === 'null') chai.assert.isTrue(sb.isEmpty(data[key]), "is null");
+        else if (key === 'undefined') chai.assert.isTrue(sb.isEmpty(data[key]), "is undefined");
+        else if (key === 'emptstr') chai.assert.isTrue(sb.isEmpty(data[key]), "is undefined");
+        else chai.assert.isNotTrue(sb.isEmpty(data[key]), "not is true" + key);
+      }
+    });
+  });
+
   describe('sb.isNull', function () {
     it('Check value is null', function () {
       for (var key in data) {
@@ -90,26 +105,12 @@ describe('Type', function () {
     });
   });
 
-/*
   describe('sb.isNaN', function () {
     it('Check value is not a number', function () {
       for (var key in data) {
-        if (key === 'num' || key === 'int' || key === 'bool' || key === 'null') chai.assert.isFalse(sb.isNaN(data[key]));
-        else chai.assert.isNotTrue(sb.isNaN(data[key]));
+        if (key === "num" || key === "int" || key === "bool" || key === "null" || key === "emptarr" || key === "emptstr") chai.assert.isFalse(sb.isNaN(data[key]), "want false -> " + key);
+        else chai.assert.isTrue(sb.isNaN(data[key]), "want true -> " + key);
       }
-    });
-  });
-*/
-
-  describe('sb.isEmpty()', function () {
-    it('Check is empty', function () {
-      chai.assert.isNotTrue(sb.isEmpty('a'), 'is string');
-      chai.assert.isNotTrue(sb.isEmpty(['a']), 'is array');
-      chai.assert.isNotTrue(sb.isEmpty({'a': 'a'}), 'is object');
-      chai.assert.isTrue(sb.isEmpty(null), 'is null');
-      chai.assert.isTrue(sb.isEmpty(undefined), 'is null');
-      //chai.assert.isTrue(sb.isEmpty([]), 'is empty array')
-      chai.assert.isTrue(sb.isEmpty(""), 'is empty string');
     });
   });
 
@@ -117,7 +118,7 @@ describe('Type', function () {
     it('Check is undefined', function () {
       for (var key in data) {
         if (key === 'undefined') chai.assert.isTrue(sb.isUndefined(data[key]));
-        else chai.assert.isNotTrue(sb.isUndefined(data[key]));
+        else chai.assert.isNotTrue(sb.isUndefined(data[key]), "assert fails in " + key);
       }
     });
   });
