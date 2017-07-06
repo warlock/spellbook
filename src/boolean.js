b11c06ee2f8c8b642bb98a8d2df8344e280833a2
Josep Subils <josep@spellbook.io>
2017-07-06 10:46:13 +0200

    [add] Add "and" method

--- src/boolean.js
+++ src/boolean.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-   /**
+  /**
    * @param {Boolean} bool : Get de boolean value.
    * @returns {Boolean} : Return true if value is true.
    */
@@ -8,7 +8,7 @@ module.exports = {
     return bool === true;
   },
 
-   /**
+  /**
    * @param {Boolean} bool : Get de boolean value.
    * @returns {Boolean} : Return true if value is false.
    */
@@ -16,7 +16,7 @@ module.exports = {
     return bool === false;
   },
 
-   /**
+  /**
    * @param {Any} value : Get any value.
    * @param {Any} check : Get any value.
    * @returns {Boolean} : Return result of comparision.
@@ -25,11 +25,15 @@ module.exports = {
     return JSON.stringify(value) === JSON.stringify(check);
   },
 
-   /**
+  /**
    * @param {Boolean} bool : Get de boolean value.
    * @returns {Boolean} : Return negative value.
    */
   'Not': function (bool) {
-    return !bool;
+    return bool === false;
+  },
+  'and': function (bool, data) {
+    if (bool && data) return true;
+    else return false;
   }
 };
