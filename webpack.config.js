299b8a20fb7d1b01a4e7cdf3e96535af128ff9c2
Josep Subils <js@js.gl>
2018-10-25 08:46:26 +0200

    [up] new versions babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,16 +11,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['babel-preset-env']
+            presets: ['@babel/preset-env']
           }
         }
       }
     ]
-
   }
 }
